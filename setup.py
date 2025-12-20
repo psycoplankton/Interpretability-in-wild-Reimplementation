27d62a1ea922a15c90bc7620b333c11056d8242e
Vansh <vansh1401876@gmail.com>
2025-12-20 20:16:48 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="easy_transformer",
+    version="0.1.0",
+    packages=find_packages(),
+    license="LICENSE",
+    description="An implementation of transformers tailored for mechanistic interpretability.",
+    long_description=open("README.md").read(),
+    install_requires=[
+        "einops",
+        "numpy",
+        "torch",
+        "datasets",
+        "transformers",
+        "tqdm",
+        "pandas",
+        "datasets",
+        "wandb",
+        "fancy_einsum",
+        "matplotlib",
+        "IPython",
+    ],
+)
